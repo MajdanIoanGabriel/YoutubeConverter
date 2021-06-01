5e30c641fb1834d63670c16cf83214fd1131d0e4
Majdan Gabriel <ioan.majdan00@e-uvt.ro>
2021-06-01 10:44:12 +0300

    Added link validation

--- YoutubeConverter/ConvertPage.xaml.cs
+++ YoutubeConverter/ConvertPage.xaml.cs
@@ -30,19 +30,37 @@ namespace YoutubeConverter
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            var idLength = 11;  // Youtube video ID lenght is 11
             var id = Input.Text;
+            var pos = id.IndexOf("v=");
+
+            if (pos < 0 || (id.Length < pos + 2 + idLength))
+            {
+                MessageBox.Show("Invalid link given!", "Error");
+                return;
+            }
+
+            id = id.Substring(pos + 2, idLength);
+
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://download-video-youtube1.p.rapidapi.com/mp3/" + id),
                 Headers =
-    {
-        { "x-rapidapi-key", "646a7edddbmsh2e7804d95936819p1a928djsn76645304dbce" },
-        { "x-rapidapi-host", "download-video-youtube1.p.rapidapi.com" },
-    },
+                {
+                    { "x-rapidapi-key", "646a7edddbmsh2e7804d95936819p1a928djsn76645304dbce" },
+                    { "x-rapidapi-host", "download-video-youtube1.p.rapidapi.com" },
+                },
             };
             using var response = await client.SendAsync(request);
+            try { 
+                response.EnsureSuccessStatusCode(); 
+            }
+            catch(Exception) {
+                MessageBox.Show("Invalid link given!", "Error");
+                return;
+            }
             response.EnsureSuccessStatusCode();
             var body = await response.Content.ReadAsStringAsync();
             System.IO.File.WriteAllText(@"D:\Facultate\Semestrul II\.NET\YoutubeConverter\response.json", body);
